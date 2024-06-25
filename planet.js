b365007d925ae5f2b51587b3ebb57cdc4d417487
Vu Nguyen <vunguyen@group5.com>
2024-06-25 14:45:14 -0400

    added in list of characters and films

--- planet.js
+++ planet.js
@@ -3,12 +3,16 @@ let terrainSpan;
 let climateSpan;
 let populationSpan;
 let planetNameH1;
+let filmsSection;
+let charactersSection;
 
 addEventListener("DOMContentLoaded", () => {
     terrainSpan = document.querySelector("span#terrain");
     climateSpan = document.querySelector("span#climate");
     populationSpan = document.querySelector("span#population");
     planetNameH1 = document.querySelector("h1#name");
+    charactersSection = document.querySelector("section#characters");
+    filmsSection = document.querySelector("section#films");
 
     const sp = new URLSearchParams(window.location.search);
     const id = sp.get('id');
@@ -20,6 +24,8 @@ async function getPlanet(planetId) {
     var planetObj;
     try {
         planetObj = await fetchPlanet(planetId);
+        planetObj.characters = await fetchCharacters(planetObj);
+        planetObj.films = await fetchFilms(planetObj);
     }
     catch (ex) {
         console.error(`Error loading planet ${planetId}`, ex.message);
@@ -33,6 +39,20 @@ async function fetchPlanet(planetid) {
     return await fetch(planetUrl).then(res => res.json());
 };
 
+async function fetchCharacters(planetObj) {
+    const url = `${baseUrl}/planets/${planetObj?.id}/characters`;
+    const characters = await fetch(url)
+        .then(res => res.json());
+    return characters;
+}
+
+async function fetchFilms(planetObj) {
+    const url = `${baseUrl}/planets/${planetObj?.id}/films`;
+    const films = await fetch(url)
+      .then(res => res.json())
+    return films;
+}
+
 const renderPlanet = planetObj => {
     document.title = `SWAPI - ${planetObj?.name}`;  // Just to make the browser tab say their name
 
@@ -40,4 +60,10 @@ const renderPlanet = planetObj => {
     terrainSpan.textContent = planetObj?.terrain;
     climateSpan.textContent = planetObj?.climate;
     populationSpan.textContent = planetObj?.population;
+
+    const characterList = planetObj?.characters?.map(char => `<li><a href="/characters.html?id=${char.id}">${char.name}</li>`)
+    charactersSection.innerHTML = characterList.join("");
+
+    const filmsList = planetObj?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
+    filmsSection.innerHTML = filmsList.join("");
 }
